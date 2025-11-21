1e80ba07e2bb65b820058f17f796d7fb305e64c6
jyt6640 <jyt6640@gmail.com>
2025-11-21 21:56:44 +0900

    feat(InputParser): 입력 형식이 올바르지 않으면 예외 발생 기능 구현

--- src/main/java/soccer/util/InputParser.java
+++ src/main/java/soccer/util/InputParser.java
@@ -4,10 +4,18 @@ public class InputParser {
     private static final String DELIMITER = " ";
     private static final int TEAM_NAME_INDEX = 0;
     private static final int SCORE_INDEX = 1;
+    private static final String INPUT_REGEX = "^\\S+ \\d+$";
 
     public MatchInput parse(String input) {
+        validate(input);
         String[] parts = input.strip()
                 .split(DELIMITER);
         return new MatchInput(parts[TEAM_NAME_INDEX], Integer.parseInt(parts[SCORE_INDEX]));
+    }
+
+    private void validate(String input) {
+        if (!input.matches(INPUT_REGEX)) {
+            throw new IllegalArgumentException("[ERROR] 입력 형식이 올바르지 않습니다.");
+        }
     }
 }
