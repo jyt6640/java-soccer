1d3ad42d55e2b7aea9ef8382a28f80a93b3d2f5e
jyt6640 <jyt6640@gmail.com>
2025-11-21 20:46:07 +0900

    init: java-soccer 초기 설정

---
+++ src/test/java/soccer/ApplicationTest.java
@@ -0,0 +1,35 @@
+package soccer;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ApplicationTest extends NsTest {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    @Test
+    void 기능_테스트() {
+        assertSimpleTest(() -> {
+            run("팀A 2, 팀B 1, 종료");
+            assertThat(output()).contains("팀A 승리");
+        });
+    }
+
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("팀A -1, 팀B 0, 종료"))
+                .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
