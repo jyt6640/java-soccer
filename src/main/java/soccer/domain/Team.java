0fb65c661287bafb876d720181526687a2b9b735
jyt6640 <jyt6640@gmail.com>
2025-11-23 15:16:26 +0900

    feat(Team): 팀 이름 10자 이상 입력 시 예외 발생 기능 구현

--- src/main/java/soccer/domain/Team.java
+++ src/main/java/soccer/domain/Team.java
@@ -4,6 +4,17 @@ public class Team {
     private String teamName;
 
     public Team(String teamName) {
+        validate(teamName);
         this.teamName = teamName;
+    }
+
+    public void validate(String teamName) {
+        nameLength(teamName);
+    }
+
+    private void nameLength(String teamName) {
+        if (teamName.length() > 10) {
+            throw new IllegalArgumentException("[ERROR] 팀 이름은 10자 미만으로 입력해주세요.");
+        }
     }
 }
