3e6367d5a7ddf5e1e04a5719c3842789dce5512e
jyt6640 <jyt6640@gmail.com>
2025-11-23 15:28:52 +0900

    feat(Score): 100점 이상 입력 시 예외 발생 기능 구현

--- src/main/java/soccer/domain/Score.java
+++ src/main/java/soccer/domain/Score.java
@@ -10,11 +10,18 @@ public class Score {
 
     public void validate(int score) {
         minusScore(score);
+        maxScore(score);
     }
 
     private void minusScore(int score) {
         if (score < 0) {
             throw new IllegalArgumentException("[ERROR] 점수는 음수 일 수 없습니다.");
+        }
+    }
+
+    private void maxScore(int score) {
+        if (score >= 100) {
+            throw new IllegalArgumentException("[ERROR] 점수는 100점 이상 일 수 없습니다.");
         }
     }
 }
