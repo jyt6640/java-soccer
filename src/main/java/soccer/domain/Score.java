064da4fdf751e12b94cbb9424bba33dd15810209
jyt6640 <jyt6640@gmail.com>
2025-11-23 15:24:39 +0900

    feat(Score): 음수 값 입력 시 예외 발생 기능 구현

--- src/main/java/soccer/domain/Score.java
+++ src/main/java/soccer/domain/Score.java
@@ -4,6 +4,17 @@ public class Score {
     private final int score;
 
     public Score(int score) {
+        validate(score);
         this.score = score;
+    }
+
+    public void validate(int score) {
+        minusScore(score);
+    }
+
+    private void minusScore(int score) {
+        if (score < 0) {
+            throw new IllegalArgumentException("[ERROR] 점수는 음수 일 수 없습니다.");
+        }
     }
 }
