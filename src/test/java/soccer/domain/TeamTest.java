39a525c1c4ed8d07f1f4fc3ea5d8be5800028729
jyt6640 <jyt6640@gmail.com>
2025-11-23 15:16:09 +0900

    test(TeamTest): 팀 이름 10자 이상 입력 시 예외 발생 테스트 추가

--- src/test/java/soccer/domain/TeamTest.java
+++ src/test/java/soccer/domain/TeamTest.java
@@ -1,6 +1,7 @@
 package soccer.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,22 @@ public class TeamTest {
     @DisplayName("팀 이름으로 Team 객체 생성")
     @Test
     void 팀_이름으로_Team_객체_생성() {
-        //input
+        //given
         String input = "팀A";
 
         //when&then
         assertThatCode(() -> new Team(input))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("팀 이름 10자 이상 입력 시 예외 발생")
+    @Test
+    void 팀_이름_10자_이상_입력_시_예외_발생() {
+        //given
+        String input = "pobiwonijeong";
+
+        assertThatThrownBy(() -> new Team(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 팀 이름은 10자 미만으로 입력해주세요.");
     }
 }
